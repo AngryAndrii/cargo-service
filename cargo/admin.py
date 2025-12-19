fc32e3ecea9770ac741591871c7ceef47a7332ac
Andrii Chychkan <mywork1728@gmail>
2025-12-19 14:10:34 +0200

    changed admin.py

--- cargo/admin.py
+++ cargo/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 from cargo.models import Truck, Service, Order, Manufacturer
 
-admin.site.register(get_user_model())
+driver = get_user_model()
+
+@admin.register(driver)
+class DriverAdmin(UserAdmin):
+    model = driver
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number", "money", "truck")}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "license_number",
+                )
+            },
+        ),
+    )
 admin.site.register(Truck)
 admin.site.register(Service)
 admin.site.register(Order)
