6819225019815206ea2a0a00a009c1a5fa80fc90
Andrii Chychkan <mywork1728@gmail>
2026-01-10 23:38:40 +0200

    added order in driver detail page

--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,22 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
+
 class DriverLIstView(LoginRequiredMixin, generic.ListView):
     model = get_user_model()
     template_name = "accounts/driver_list.html"
+
+    def get_queryset(self):
+        return get_user_model().objects.select_related(
+            "truck", "truck__manufacturer"
+        )
 
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = get_user_model()
     template_name = "accounts/driver_detail.html"
 
-    # def get_queryset(self):
-    #     return Manufacturer.objects.prefetch_related(
-    #         "trucks"
-    #     )
+    def get_queryset(self):
+        return get_user_model().objects.select_related(
+            "truck", "truck__manufacturer"
+        )
