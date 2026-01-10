3e729a4a94eb06a368c7adb3f35c9c7dd8bd74f0
Andrii Chychkan <mywork1728@gmail>
2026-01-10 18:32:11 +0200

    added truck condition

--- cargo/views.py
+++ cargo/views.py
@@ -1,11 +1,5 @@
-import datetime
-
 from django import forms
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
-from django.db.models import Count
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
